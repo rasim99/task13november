760241ac107b069ef48f4c3b24fc63318812de66
rasim99 <rasimjm@code.edu.az>
2022-11-15 08:10:17 -0800

    task13nov

---
+++ task13nov/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace task13nov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //STring1("ADil");  
+            //Console.WriteLine(STring2("ADlAr"));
+            //string name3 = ($"   Lorem  ");
+            //Console.WriteLine(STring3(name3));
+            //STring4("Lorem");
+            //string name5 = ("Rasif");
+            //Console.WriteLine(STring5(name5));
+            //string name6 = ("DoLeL");
+            //STring6(name6);
+            //string name7 = ("RabbiT");
+            //STring7(name7);
+            //string newARr = ("hello world");
+            //Console.WriteLine(REverse(newARr));
+           
+        }
+        //static void STring1(string name1)
+        //{
+        //    Console.WriteLine($"{name1}".ToUpper());
+        //}
+        //static string STring2(string name2)
+        //{
+        //    return name2.ToLower();
+        //}
+        //static string STring3(string name3)
+        //{
+        //    return name3.TrimStart();
+        //}
+        //static void STring4(string name4)
+        //{
+
+        //    Console.WriteLine($"{name4}".StartsWith("B"));
+        //}
+        //static string STring5(string name5)
+        //{
+        //    return name5.Substring(1);
+        //}
+        //static void STring6(string name6)
+        //{
+        //    Console.WriteLine($"name6".Contains("L"));
+              
+        //}
+        //static void STring7(string name7)
+        //{
+        //    Console.WriteLine($"{name7}".EndsWith("T")); 
+        //}
+       //static string REverse(string words)
+       //{
+       //     string[] Arr = words.Split("");
+       //     string newARr = String.Empty;
+       //     for (int i = 0; i <Arr.Length; i++)
+       //     {
+       //         string sum = String.Empty;
+       //         for (int k = Arr[i].Length-1; k >=0; k--)
+       //         {
+       //             sum += Arr[i][k];
+                    
+       //         }
+       //         newARr += sum;
+       //     }
+       //     return newARr;
+       }
+    }
+}
